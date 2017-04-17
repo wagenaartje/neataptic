58b38f0b853b95c6d86dad38adc841b09f348201
Thomas Wagenaar <wagenaartje@protonmail.com>
2017-04-17 13:15:11 +0200

    prebuild remove

--- prebuild.js
+++ prebuild.js
@@ -1,16 +1,5 @@
 var fs = require('fs')
 module.exports = function(){
   // LICENSE
-  var license = fs.readFileSync('LICENSE', 'utf-8')
-
-  // Update the README to include the (one to latest) dist version
-  var commithash = fs.readFileSync('.git/refs/heads/master', 'utf-8')
-  commithash = commithash.substring(0, 8)
-
-  var readme = fs.readFileSync('README.md', 'utf-8')
-  .replace(/cdn.rawgit.com\/wagenaartje\/neataptic\/(.*)\/dist/, `cdn.rawgit.com/wagenaartje/neataptic/${commithash}/dist`)
-  fs.writeFileSync('README.md', readme);
-
-  // Return license for dist banner
-  return license;
+  return fs.readFileSync('LICENSE', 'utf-8');
 }

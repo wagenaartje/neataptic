4dc7e39863fa1c6ffc9dede73d4aed0f9294e004
Thomas Wagenaar <wagenaartje@protonmail.com>
2017-05-03 19:21:21 +0200

    Added 4 cost methods
    * Methods.Cost.MAE
    * Methods.Cost.MAPE
    * Methods.Cost.MSLE
    * Methods.Cost.HINGE

--- docs/cdn/neataptic.js
+++ docs/cdn/neataptic.js
@@ -2462,25 +2462,68 @@ if (module) module.exports = Connection;
 
 // https://en.wikipedia.org/wiki/Loss_function
 var Cost = {
-  CROSS_ENTROPY: function(target, output)
-  {
-    var crossentropy = 0;
-    for (var i in output)
-      crossentropy -= target[i] * Math.log(output[i]+1e-15) + (1-target[i]) * Math.log((1+1e-15) - output[i]); // +1e-15 is a tiny push away to avoid Math.log(0)
-    return crossentropy;
+  // Cross entropy error
+  CROSS_ENTROPY: function(target, output){
+    var error = 0;
+    for(var i in output){
+      // Avoid negative and zero numbers, use 1e-15 http://bit.ly/2p5W29A
+      error -= target[i] * Math.log(Math.max(output[i],1e-15)) + (1-target[i]) * Math.log(1 - Math.max(output[i],1e-15));
+    }
+    return error;
   },
-  MSE: function(target, output)
-  {
-    var mse = 0;
-    for (var i in output)
-      mse += Math.pow(target[i] - output[i], 2);
-    return mse / output.length;
+  // Mean Squared Error
+  MSE: function(target, output){
+    var error = 0;
+    for(var i in output){
+      error += Math.pow(target[i] - output[i], 2);
+    }
+
+    return error / output.length;
   },
+  // Binary error
   BINARY: function(target, output){
     var misses = 0;
-    for (var i in output)
+    for(var i in output){
       misses += Math.round(target[i] * 2) != Math.round(output[i] * 2);
+    }
+
     return misses;
+  },
+  // Mean Absolute Error
+  MAE: function(target, output){
+    var error = 0;
+    for(var i in output){
+      error += Math.abs(target[i] - output[i]);
+    }
+
+    return error / output.length;
+  },
+  // Mean Absolute Percentage Error
+  MAPE: function(target, output){
+    var error = 0;
+    for(var i in output){
+      error += Math.abs((output[i] - target[i]) / Math.max(target[i], 1e-15));
+    }
+
+    return error / output.length;
+  },
+  // Mean Squared Logarithmic Error
+  MSLE: function(target, output){
+    var error = 0;
+    for(var i in output){
+      error += Math.log(Math.max(target[i], 1e-15)) - Math.log(Math.max(output[i], 1e-15));
+    }
+
+    return error;
+  },
+  // Hinge loss, for classifiers
+  HINGE: function(target, output){
+    var error = 0;
+    for(var i in output){
+      error += Math.max(0, 1 - target[i] * output[i]);
+    }
+
+    return error;
   }
 };
 

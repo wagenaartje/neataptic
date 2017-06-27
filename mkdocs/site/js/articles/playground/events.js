8e630454321e2a505ff85c47d34795be4b9da9d9
Thomas Wagenaar <wagenaartje@protonmail.com>
2017-06-27 14:12:58 +0200

    remove mkdocs build

--- mkdocs/site/js/articles/playground/events.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-  $( ".start" ).click(function() {
-    $( ".start" ).html('<span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span> Running...');
-    newNeat();
-    loop();
-  });
-});

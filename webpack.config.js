6d4636eab6fd5f274e66cf9814f263107c0b064e
Thomas Wagenaar <wagenaartje@protonmail.com>
2017-07-30 20:59:42 +0200

    Compiling file
    Planning to move mkdocs and docs to the gh-pages branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
-var webpack = require('webpack')
-var license = require('./prebuild.js')
-var version = require('./package.json').version
+/* Import */
+var fs = require('fs');
+var webpack = require('webpack');
+var version = require('./package.json').version;
 
-var data = {
+/* Update readme and read license */
+var readme = fs.readFileSync('README.md', 'utf-8').replace(
+  /cdn\/(.*)\/neataptic.js/, `cdn/${version}/neataptic.js`
+);
+fs.writeFileSync('README.md', readme);
+
+var license = fs.readFileSync('LICENSE', 'utf-8');
+
+/* Export config */
+module.exports = {
   context: __dirname,
   entry: {
     'dist/neataptic': './src/neataptic.js',
+    [`mkdocs/theme/cdn/${version}/neataptic`]: './src/neataptic.js'
   },
   output: {
     path: './',
@@ -15,9 +26,6 @@ var data = {
   },
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.BannerPlugin(license())
+    new webpack.BannerPlugin(license)
   ]
-}
-
-data.entry[`mkdocs/theme/cdn/${version}/neataptic`] = './src/neataptic.js'
-module.exports = data;
+};
